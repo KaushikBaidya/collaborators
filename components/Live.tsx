a0b10f1c38318a2c1e16d2fb7c286f69b7fbebf7
KAUSHIK BAIDYA <47146123+KaushikBaidya@users.noreply.github.com>
2024-02-05 18:19:43 +0600

    cursor chat box created

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,10 +1,19 @@
-import { useCallback } from 'react';
-import { useMyPresence, useOthers } from '@/liveblocks.config';
+import { useCallback, useEffect, useState } from 'react';
+import {
+	useMyPresence,
+	useOthers,
+	useUpdateMyPresence,
+} from '@/liveblocks.config';
 import LiveCursors from './cursor/LiveCursors';
+import CursorChat from './cursor/CursorChat';
+import { CursorMode } from '@/types/type';
 
 const Live = () => {
 	const others = useOthers();
 	const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+	const [cursorState, setCursorState] = useState({
+		mode: CursorMode.Hidden,
+	});
 
 	const handlePointerMove = useCallback((event: React.PointerEvent) => {
 		event.preventDefault();
@@ -16,7 +25,7 @@ const Live = () => {
 	}, []);
 
 	const handlePointerLeave = useCallback((event: React.PointerEvent) => {
-		event.preventDefault();
+		setCursorState({ mode: CursorMode.Hidden });
 
 		updateMyPresence({ cursor: null, message: null });
 	}, []);
@@ -28,6 +37,35 @@ const Live = () => {
 		updateMyPresence({ cursor: { x, y } });
 	}, []);
 
+	useEffect(() => {
+		const onKeyUp = (e: KeyboardEvent) => {
+			if (e.key === '/') {
+				setCursorState({
+					mode: CursorMode.Chat,
+					previousMessage: null,
+					message: '',
+				});
+			} else if (e.key === 'Escape') {
+				updateMyPresence({ message: '' });
+				setCursorState({ mode: CursorMode.Hidden });
+			}
+		};
+
+		const onKeyDown = (e: KeyboardEvent) => {
+			if (e.key === '/') {
+				e.preventDefault();
+			}
+		};
+
+		window.addEventListener('keyup', onKeyUp);
+		window.addEventListener('keydown', onKeyDown);
+
+		return () => {
+			window.removeEventListener('keyup', onKeyUp);
+			window.removeEventListener('keydown', onKeyDown);
+		};
+	}, [updateMyPresence]);
+
 	return (
 		<div
 			onPointerMove={handlePointerMove}
@@ -36,6 +74,16 @@ const Live = () => {
 			className='w-full h-screen flex items-center justify-center border-2 border-purple-400'
 		>
 			<h1 className='text-5xl text-white'>hello collaborators</h1>
+
+			{cursor && (
+				<CursorChat
+					cursor={cursor}
+					cursorState={cursorState}
+					setCursorState={setCursorState}
+					updateMyPresence={updateMyPresence}
+				/>
+			)}
+
 			<LiveCursors others={others} />
 		</div>
 	);
